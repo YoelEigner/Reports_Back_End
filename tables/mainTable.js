a88ea930e337850009a8bb5bda4ceb07c66d0ae4
YoelEigner <joele8323@gmail.com>
2023-01-16 15:17:37 +0200

    update

--- tables/mainTable.js
+++ tables/mainTable.js
@@ -6,8 +6,9 @@ exports.mainTable = (data, date) => {
         subtitle: "From " + date.start + " To " + date.end,
         headers: [
             { label: "Date", property: 'batch_date', renderer: null, align: "center" },
-            { label: "Funder Name", property: 'funder_name', renderer: null, align: "center" },
+            { label: "Worker", property: 'event_primary_worker_name', renderer: null, align: "center" },
             { label: "Individual Name", property: 'individual_name', renderer: null , align: "center"},
+            { label: "Funder Name", property: 'funder_name', renderer: null, align: "center" },
             { label: "Invoice ID", property: 'invoice_id', renderer: null, align: "center" },
             { label: "Service Name", property: 'service_name', renderer: null, align: "center" },
             { label: "Cart Item", property: 'event_service_item_name', renderer: null, align: "center" },
