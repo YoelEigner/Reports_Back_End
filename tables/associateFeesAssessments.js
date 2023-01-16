13afad87db64a0ae573bf44fbcd6839d4ec742dd
YoelEigner <joele8323@gmail.com>
2023-01-16 18:12:51 +0200

    update

--- tables/associateFeesAssessments.js
+++ tables/associateFeesAssessments.js
@@ -4,6 +4,7 @@ exports.associateFeesAssessments = async (worker, data, date, rate, tableType) =
     let count = data.length
     let fee = data.map(x => x.assessmentAssociateFee = x.applied_amt ? (x.applied_amt / 100) * rate : (Number(x.TOTAL.replace(/[^0-9.-]+/g, "")) / 100) * rate).reduce((a, b) => a + b, 0)
     // let HST = (fee / 100) * process.env.HST
+    let totalOfAllItems = data.map(x => x.totalOfAllItems = x.applied_amt ? x.applied_amt : x.TOTAL).reduce((a, b) => a + b, 0)
     let hst = fee * (process.env.HST / 100)
 
     return {
@@ -12,6 +13,7 @@ exports.associateFeesAssessments = async (worker, data, date, rate, tableType) =
         headers: [
             { label: "Worker", renderer: null, align: "center" },
             { label: "Quantity", renderer: null, align: "center" },
+            { label: "Total Assessment", renderer: null, align: "center" },
             { label: "Fee Base Rate", renderer: null, align: "center" },
             { label: "HST", renderer: null, align: "center" },
             { label: "Total + HST", renderer: null, align: "center" }
@@ -20,6 +22,7 @@ exports.associateFeesAssessments = async (worker, data, date, rate, tableType) =
             [
                 worker,
                 count,
+                formatter.format(totalOfAllItems),
                 `${rate}%`,
                 formatter.format(hst),
                 formatter.format(fee + hst)
