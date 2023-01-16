cdbe6b356321a3cb3c798ee4a5ad638a4b009386
YoelEigner <joele8323@gmail.com>
2023-01-16 13:35:06 +0200

    updated

--- tables/appliedPaymentsTable.js
+++ tables/appliedPaymentsTable.js
@@ -1,11 +1,12 @@
-const { formatter } = require("../pdfWriter/pdfKitFunctions")
+const { formatter, sortByDate, sortByJustDate, sortByName, sortByDateAndName } = require("../pdfWriter/pdfKitFunctions")
 const { getRate } = require("./associateFeesTherapy")
 const moment = require('moment')
 
 exports.appliedPaymentsTable = async (date, paymentData, workerId) => {
+
     paymentData.map(x => {
         x.superviser = x.superviser.split(',')[1] + " " + x.superviser.split(',')[0]
-        x.batch_date = moment(x.batch_date).format('YYYY-MM-DD')
+        x.batch_date = moment(x.FULLDATE).format('YYYY-MM-DD')
     })
     let totalAppliedAmt = paymentData.map(x => Number(x.applied_amt)).reduce((a, b) => a + b, 0)
     let totalDuration_hrs = paymentData.map(x => Number(x.duration_hrs)).reduce((a, b) => a + b, 0)
@@ -24,14 +25,17 @@ exports.appliedPaymentsTable = async (date, paymentData, workerId) => {
         paymentData.map(x => x.subPracAmount = Number(rate.associateRate) <= 0 ? formatter.format(0) : formatter.format(Number(rate.associateRate))).reduce((a, b) => a + b, 0)
         subPracTotal = paymentData.map(x => Number(x.subPracAmount.replace(/[^0-9.-]+/g, ""))).reduce((a, b) => a + b, 0)
     }
+
+    sortByDateAndName(paymentData)
+
     let headers = [
-        { label: "Date", property: 'batch_date',   align: "center" },
+        { label: "Date", property: 'batch_date', align: "center" },
         { label: "Individual Name", property: 'service_file_presenting_individual_name', align: "center" },
         { label: "Service Name", property: 'case_program', align: "center" },
         { label: "Cart Item", property: 'description', align: "center" },
         { label: "Invoice ID", property: 'inv_no', align: "center" },
         { label: "Record ID", property: 'rec_id', align: "center" },
-        { label: "Worker", property: 'worker',  align: "center" },
+        { label: "Worker", property: 'worker', align: "center" },
         { label: "Superviser", property: 'superviser', align: "center" },
         { label: "Qty Charged", property: 'duration_hrs', align: "center" },
         { label: "Applied Amount", property: 'applied_amt', align: "center" },
@@ -49,9 +53,7 @@ exports.appliedPaymentsTable = async (date, paymentData, workerId) => {
         subtitle: "From " + date.start + " To " + date.end,
         headers: headers,
         datas: [...paymentData],
-        rows: [
-            rows,
-        ],
+        rows: [rows],
         L1AssociateFee: associateFee
     }
 }
--- tables/superviseeClientPaymentsTable.js
+++ tables/superviseeClientPaymentsTable.js
@@ -1,18 +1,16 @@
-const { sortByName, formatter } = require("../pdfWriter/pdfKitFunctions")
+const { sortByName, formatter, sortByDateAndName } = require("../pdfWriter/pdfKitFunctions")
 
 
 exports.superviseeClientPaymentsTable = (date, data) => {
-    sortByName(data)
-    // console.log(data)
-    // let totalQty = data.map(x => x.qty).reduce((a, b) => a + b, 0)
-    // let totalAppliedPayments = data.map(x => Number(x.total.replace(/[^0-9.-]+/g, ""))).reduce((a, b) => a + b, 0)
+    sortByDateAndName(data)
     return {
         title: "Supervisee Total",
         subtitle: "From " + date.start + " To " + date.end,
         headers: [
             { label: "Supervisee", property: 'worker', renderer: null, align: "center" },
             { label: "Cart Item", property: 'description', renderer: null, align: "center" },
-            { label: "Qty", property: 'duration_hrs', renderer: null, align: "center" },
+            { label: "Invoice Number", property: 'inv_no', renderer: null, align: "center" },
+            { label: "Qty", property: 'qty', renderer: null, align: "center" },
             { label: "Item Total", property: 'applied_amtTemp', renderer: null, align: "center" },
             { label: "Total Payments Applied", property: 'totalTemp', renderer: null, align: "center" },
         ],
