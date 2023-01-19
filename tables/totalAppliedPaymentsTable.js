f4845ca0159df24f00fc1f1a190659a09b0f781e
YoelEigner <joele8323@gmail.com>
2023-01-19 15:07:27 +0200

    updated

--- tables/totalAppliedPaymentsTable.js
+++ tables/totalAppliedPaymentsTable.js
@@ -1,11 +1,13 @@
 const { formatter } = require("../pdfWriter/pdfKitFunctions")
 
 exports.totalAppliedPaymentsTable = (date, clientPayments, clientHours, superviseeClientsPayment, superviseeClientsHours, ajustmentFeesTotal, totalAppliedAmount, totalSupPracAmount, totalSupPraHours, totalAppliedHrs) => {
-    let superviseePaymentAmt = (superviseeClientsPayment - totalAppliedAmount) + totalSupPracAmount
-    let superviseeHrs = (superviseeClientsHours - totalAppliedHrs) + totalSupPraHours
-    let total = clientPayments + superviseePaymentAmt + ajustmentFeesTotal
+    // let superviseePaymentAmt = (superviseeClientsPayment - totalAppliedAmount) + totalSupPracAmount
+    // let superviseeHrs = (superviseeClientsHours - totalAppliedHrs) + totalSupPraHours
+    // let total = clientPayments + superviseePaymentAmt + ajustmentFeesTotal
+    // let total = clientPayments - superviseeClientsPayment - totalAppliedAmount - totalSupPracAmount + ajustmentFeesTotal
 
-    // let total = clientPayments + ((superviseeClientsPayment - totalAppliedAmount) + totalSupPracAmount) + -190 
+
+    let total = clientPayments + ((superviseeClientsPayment - totalAppliedAmount) + totalSupPracAmount) + ajustmentFeesTotal 
 
     return {
         title: "Applied Payments Total",
@@ -21,11 +23,39 @@ exports.totalAppliedPaymentsTable = (date, clientPayments, clientHours, supervis
         rows: [[
             formatter.format(clientPayments),
             clientHours.toFixed(0),
-            formatter.format(superviseePaymentAmt),
-            superviseeHrs.toFixed(0),
+            formatter.format((superviseeClientsPayment - totalAppliedAmount) + totalSupPracAmount),
+            superviseeClientsHours.toFixed(0),
             formatter.format(ajustmentFeesTotal),
             formatter.format(total),
         ]],
     }
 }
+
+// exports.totalAppliedPaymentsTable = (date, clientPayments, clientHours, superviseeClientsPayment, superviseeClientsHours, ajustmentFeesTotal, totalAppliedAmount, totalSupPracAmount) => {
+//     let total = clientPayments - superviseeClientsPayment - totalAppliedAmount - totalSupPracAmount + ajustmentFeesTotal
+//     // let total = clientPayments + ((superviseeClientsPayment - totalAppliedAmount) + totalSupPracAmount) + -190 
+//     return {
+//         title: "Applied Payments Total",
+//         subtitle: "From " + date.start + " To " + date.end,
+//         headers: [
+//             { label: "Client Payments", renderer: null, align: "center" },
+//             { label: "Client Hours", renderer: null, align: "center" },
+//             { label: "Payments From Supervisee's clients", renderer: null, align: "center" },
+//             { label: "Hours From Supervisee's clients", renderer: null, align: "center" },
+//             { label: "Adjustment Fees", renderer: null, align: "center" },
+//             { label: "Total Applied Payments", renderer: null, align: "center" }
+//         ],
+//         rows: [[
+//             formatter.format(clientPayments),
+//             clientHours.toFixed(0),
+//             formatter.format((superviseeClientsPayment - totalAppliedAmount) + totalSupPracAmount),
+//             superviseeClientsHours.toFixed(0),
+//             formatter.format(ajustmentFeesTotal),
+//             formatter.format(total),
+//         ]],
+//     }
+// }
+
+
+
 
