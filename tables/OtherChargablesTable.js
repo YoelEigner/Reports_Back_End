5b17785e766e53740f55b270e0287a587646a584
YoelEigner <35924447+YoelEigner@users.noreply.github.com>
2024-02-27 16:56:24 +0200

    Update others table

--- tables/OtherChargablesTable.js
+++ tables/OtherChargablesTable.js
@@ -30,7 +30,6 @@ exports.OtherChargablesTable = (data, date, otherItems, workerProfile) => {
             { label: "Date", property: 'batch_date', renderer: null, align: "center" },
             { label: "Worker", property: 'event_primary_worker_name', renderer: null, align: "center" },
             { label: "Individual Name", property: 'individual_name', renderer: null, align: "center" },
-            { label: "Funder Name", property: 'funder_name', renderer: null, align: "center" },
             { label: "Invoice ID", property: 'invoice_id', renderer: null, align: "center" },
             { label: "Service Name", property: 'service_name', renderer: null, align: "center" },
             { label: "Cart Item", property: 'event_service_item_name', renderer: null, align: "center" },
@@ -40,7 +39,7 @@ exports.OtherChargablesTable = (data, date, otherItems, workerProfile) => {
         ],
         datas: [...data],
         rows: [
-            ['Total', '-', '-', '-', '-', '-', '-', '-', totalQty, formatter.format(totalAmt)],
+            ['Total', '-', '-', '-', '-', '-', '-', totalQty, formatter.format(totalAmt)],
         ],
     }
 };
